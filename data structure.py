3b44539b822a20bac37e308829f3963532ce76b2
avinash-coding-journal <144519715+AVINASHA2023@users.noreply.github.com>
2025-08-04 11:43:36 +0530

    Data types & Operators

---
+++ data structure.py
@@ -0,0 +1,113 @@
+# print("list")
+# print("__________")
+# a=["apple","orange","grapes"]
+# print(a)     #example
+
+
+# print("__________")
+# a=["apple","orange","grapes"]
+# print(a)
+# print(len(a)) #length function
+
+# print("list")
+# print("__________")
+# a=["apple","orange","grapes"]
+# print(a)
+# print(type(a))  #type function
+
+
+# print("list")
+# print("__________")
+# a=list(("apple","orange","grapes"))
+# print(a)
+
+# Tuple
+
+# print("Update")
+# print("______")
+# x=("apple","banana","cherry")
+# print(type(x))
+# y=list(x)
+# print(y)
+# print(type(y))
+
+# y[1]="kivi"
+# x=tuple(y)
+# print(x)
+# print(type(x))
+
+
+# print("Asterisk tuple")
+# print("______________")
+# fruits=("apple","banana","cherry","orange","pineapple","dragon fruit")
+# (green,yellow,*red,pink)=fruits
+# print(green)
+# print(yellow)
+# print(red)
+# print(pink)
+
+# print("update")
+# print("_________")
+# x=("apple","banana","cherry")
+# print(type(x))
+# y=list(x)
+# print(y)
+# print(type(y))
+# y [1]="kivi"
+# x=tuple(y)
+# print(x)
+# print(type(x))
+
+# String
+
+quantity=20
+items="Blueberry"
+price=550
+myorder= "I want to pay {2} rupees for {0} boxes of {1}"
+print(myorder.format(quantity,items,price))
+b = myorder.format(quantity,items,price)
+print(b)
+
+# print("string length")
+# print("__________")
+# a="hello world"
+# print(len(a))
+
+# print("upper case")
+# print("________")
+# a="hello world"
+# print(a.upper())
+
+# dict
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+# x = car.values()
+# print(x) #before the change
+# car["year"] = 2020
+# print(x) #after the change
+
+# print("Add items")
+# print("***********")
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964,
+# }
+# x = car.keys()
+# print(x) #before the change
+# car["color"] = "white"
+# print(x) #after the change
+# print(car)
+
+
+a=float(input("enter the side length of the cube:"))
+cube=a*3
+print("volume of the cube")
+
+
+
+
+
