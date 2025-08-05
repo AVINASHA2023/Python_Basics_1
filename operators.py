fbddfba40bb7010c9dc36dabf099d0a62b85287b
avinash-coding-journal <144519715+AVINASHA2023@users.noreply.github.com>
2025-08-05 12:02:28 +0530

    py. operators

---
+++ operators.py
@@ -0,0 +1,73 @@
+# print("logical operator")
+# print("********************")
+#
+# x=5
+# print(x<3 and x<10)
+#
+# x=5
+# print(x<3 or x<10)
+#
+# x=5
+# print(not(x<3 or x<10))
+
+# print("logical operator")
+# print("****************")
+# x=10
+# print(x<7 and x<9)
+# x=10
+# print(x<7 or x<9)
+# print(10
+# print(not(x<7 or x<9))
+
+
+
+# print("for loop")
+# print("****************")
+# fruits =["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+#     print("i like",x)
+#
+# print("range() funhction")
+# print("*************")
+# print("ex: 1")
+# for y in range (2,30,3):
+#     print(y)
+
+
+# print("Ex:2")
+# genre = ["pop", "rock", "jazz"]
+# for i in range(len(genre)):
+#     print ("I like", genre[i])
+
+
+#decision making
+# print("if")
+# print("*********")
+# num=3
+# if num>0:
+#     print(num,"is a positive number")
+
+# nested if
+# print("nested if")
+# print("**********")
+# num=-9
+# if num>=0:
+#     if num==0:
+#         print("zero")
+#     else:
+#         print(num,"positive number")
+# else:
+#     print(num,"negative number")
+
+# break statement
+
+print("brake")
+print("*********")
+fruit=["apple","banana","cherry"]
+for x in fruit:
+    if x=="banana":
+        continue
+    print(x)
+
+
